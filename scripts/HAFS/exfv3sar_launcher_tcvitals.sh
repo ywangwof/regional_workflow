d3dcb56314623d57ee87f6e7d55559b0073c170a
Henry R. Winterbottom <henry.winterbottom@noaa.gov>
2019-03-30 11:17:57 -0600

    Fixed bug occurring creation of history files for all TC events to be simulated.

--- scripts/HAFS/exfv3sar_launcher_tcvitals.sh
+++ scripts/HAFS/exfv3sar_launcher_tcvitals.sh
@@ -16,7 +16,7 @@ set -x -e
 
 # Define environment for respective experiment.
 
-. ${WORKdir}/${USER}/${CYCLE}/intercom/experiment.${CYCLE}
+. ${WORKdir}/${USER}/${EXPTname}/${CYCLE}/intercom/experiment.${CYCLE}
 
 #----
 
@@ -38,7 +38,7 @@ parse_tcv_records (){
 
     # Remove previous occurance of external file.
 
-    rm ${TCV_RECORD_FILEPATH}
+    rm ${TCV_RECORD_FILEPATH} 2> /dev/null || :
 
     # Check local variable and proceed accordingly
 
@@ -96,90 +96,90 @@ tcv_record_cycle (){
 # This function collects all previous TC-vitals records for the user
 # specified TC event.
 
-# INPUT VARIABLES:
-
-# * tcv_event; the TC event for which to collect the TC-vitals record
-#   for the current forecast cycle.
-
 tcv_record_history (){
 
-    # Get TC event from user arguments.
-
-    tcv_event=$1
-
-    # Initialize local variable.
+    # Loop through all TC events for current cycle and create a
+    # composite TC-vitals history file for each event.
     
-    tcv_check=1
+    while read -r tcv_event; do
+
+	# Initialize local variable.
     
-    # Define initial forecast cycle for TC event.
+	tcv_check=9999
+
+	# Define initial forecast cycle for TC event.
     
-    cycle=`cat ${TIMEINFO_FILEPATH} | grep -w "cycle" | awk '{print $2}'`
+	cycle=`cat ${TIMEINFO_FILEPATH} | grep -w "cycle" | awk '{print $2}'`
     
-    # Define history file for TC event TC-vitals records.
+	# Define history file for TC event TC-vitals records.
     
-    history_file=${ITRCROOT}/tcvitals.${tcv_event}.${CYCLE}.history
+	history_file=${ITRCROOT}/tcvitals.${tcv_event}.${CYCLE}.history
     
-    # Remove any previous occurances of external file.
+	# Remove any previous occurances of external file.
     
-    rm ${history_file}
+	rm ${history_file} 2> /dev/null || :
     
-    # Loop through all previous cycles until the respective TC event
-    # can no longer be traced.
+	# Loop through all previous cycles until the respective TC
+	# event can no longer be traced.
 
-    while : ; do
+	while [ ${tcv_check} -ge 1 ]; do
 
-	# Define external file to contain previous cycle time and date
-	# information.
+	    # Define external file to contain previous cycle time and
+	    # date information.
 
-	timeinfo_filepath=${LAUNCHERdir}/tcv_cycle.timeinfo
+	    timeinfo_filepath=${LAUNCHERdir}/tcv_cycle.timeinfo
 
-	# Define previous cycle information relevant to current cycle.
+	    # Define previous cycle information relevant to current
+	    # cycle.
 
-	python ${UTILdir}/date_time_format.py --date_string ${cycle} --offset_seconds -${SYNDAToffset_seconds} --output_file ${timeinfo_filepath}
+	    python ${UTILdir}/date_time_format.py --date_string ${cycle} --offset_seconds -${SYNDAToffset_seconds} --output_file ${timeinfo_filepath}
 
-	# Define previous forecast cycle time stamp attributes.
+	    # Define previous forecast cycle time stamp attributes.
 
-	year=`cat ${timeinfo_filepath} | grep -w "year" | awk '{print $2}'`
-	month=`cat ${timeinfo_filepath} | grep -w "month" | awk '{print $2}'`
-	day=`cat ${timeinfo_filepath} | grep -w "day" | awk '{print $2}'`
-	hour=`cat ${timeinfo_filepath} | grep -w "hour" | awk '{print $2}'`
-	cycle=`cat ${timeinfo_filepath} | grep -w "cycle" | awk '{print $2}'`
+	    year=`cat ${timeinfo_filepath} | grep -w "year" | awk '{print $2}'`
+	    month=`cat ${timeinfo_filepath} | grep -w "month" | awk '{print $2}'`
+	    day=`cat ${timeinfo_filepath} | grep -w "day" | awk '{print $2}'`
+	    hour=`cat ${timeinfo_filepath} | grep -w "hour" | awk '{print $2}'`
+	    cycle=`cat ${timeinfo_filepath} | grep -w "cycle" | awk '{print $2}'`
 
-	# Remove previous occurances of external file.
+	    # Remove previous occurances of external file.
 
-	rm ${timeinfo_filepath}
+	    rm ${timeinfo_filepath} 2> /dev/null || :
 
-	# Define time stamp search string for TC-vitals records.
+	    # Define time stamp search string for TC-vitals records.
 
-	tcv_timestamp="${year}${month}${day} ${hour}00"
+	    tcv_timestamp="${year}${month}${day} ${hour}00"
 	
-	# Check that the respective TC event has TC-vitals record for
-	# the previous forecast cycle.
-	
-	tcv_check=`cat ${SYNDATpath}/* | grep -w "${tcv_timestamp}" | grep -wc ${tcv_event}`
-	
-	# If no more records for the current TC event exist, break
-	# loop.
-	
-	[[ ${tcv_check}>0 ]] || break
-	
-	# Capture the most inclusive information for the current TC
-	# event for the respective previous forecast cycle and append
-	# it to the external file.
+	    # Check that the respective TC event has TC-vitals record
+	    # for the previous forecast cycle.
+	    
+	    tcv_check=`cat ${SYNDATpath}/* | grep -w "${tcv_timestamp}" | grep -wc ${tcv_event}` || :
 
-	cat ${SYNDATpath}/* | grep -w "${tcv_timestamp}" | grep -w ${tcv_event} | sort -nr | head -1 >> ${history_file}
+	    # Check local variable and proceed accordingly.
 
-    done # while [ ${tcv_check}>0 ]
+	    if [ ${tcv_check} -ge 1 ]; then
+		
+		# Capture the most inclusive information for the
+		# current TC event for the respective previous
+		# forecast cycle and append it to the external file.
 
-    # Reorder file such that the oldest time stamp is first and write
-    # to temporary file path.
+		cat ${SYNDATpath}/* | grep -w "${tcv_timestamp}" | grep -w ${tcv_event} | sort -nr | head -1 >> ${history_file}
 
-    cat ${history_file} | sort -k 4,4 > ${LAUNCHERdir}/tmp.file
+	    fi # if [ ${tcv_check} -ge 1 ]
 
-    # Rename temporary file path and replace respective TC event
-    # history file path.
+	done # while [ ${tcv_check}>0 ]
 
-    mv ${LAUNCHERdir}/tmp.file ${history_file}
+	# Reorder file such that the oldest time stamp is first and
+	# write to temporary file path.
+
+	cat ${history_file} | sort -k 4,4 > ${LAUNCHERdir}/tmp.file
+
+	# Rename temporary file path and replace respective TC event
+	# history file path.
+
+	mv ${LAUNCHERdir}/tmp.file ${history_file}
+
+    done < ${TCV_EVENTS_FILEPATH}
 }
 
 #----
@@ -197,27 +197,33 @@ tcv_record_history (){
 
 parse_tcv_records_cycle (){
 
-    # Determine all TC-vitals events for the current forecast cycle.
+    # Define file path to contain a list of the TC-vitals events for
+    # the current forecast cycle.
 
-    tcv_events=`cat ${SYNDATpath}/* | grep -E "JTWC|NHC" | grep -w "${TCV_TIMESTAMP}" | awk '{print $2}' | sort -u`
+    export TCV_EVENTS_FILEPATH=${ITRCROOT}/tcevents.${CYCLE}.info
+
+    # Write all TC-vitals events for the current forecast cycle to an
+    # external file.
+
+    cat ${SYNDATpath}/* | grep -E "JTWC|NHC" | grep -w "${TCV_TIMESTAMP}" | awk '{print $2}' | sort -u > ${TCV_EVENTS_FILEPATH}
 
     # Loop through all TC events for current cycle and create a
     # composite TC-vitals file and TC-vitals history file for each
     # event.
     
-    for tcv_event in ${tcv_events}; do
+    while read -r tcv_event; do
 
 	# Collect TC-vitals records for respective TC event for the
 	# current forecast cycle.
 
 	tcv_record_cycle ${tcv_event}
-	
-	# Create a TC-vitals records history file for the respective
-	# TC event relative to the current forecast cycle.
 
-	tcv_record_history ${tcv_event}
+    done < ${TCV_EVENTS_FILEPATH}
 
-    done # for tcv_event in ${tcv_events}
+    # Create a TC-vitals records history files for the respective TC
+    # events within the current forecast cycle.
+
+    tcv_record_history
 }
 
 #----
