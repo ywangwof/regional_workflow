902a974f15bfc237bc5d9a6eccbe00ed9409f1d2
gerard ketefian <gerard.ketefian@noaa.gov>
2019-06-26 15:43:49 -0600

    Remove commented-out code.
    Former-commit-id: b996114ea678337b4a11f4a85d58f9e5d05b6643

--- ush/generate_FV3SAR_wflow.sh
+++ ush/generate_FV3SAR_wflow.sh
@@ -112,17 +112,6 @@ HH_FIRST_CYCL=${CYCL_HRS[0]}
 #
 #-----------------------------------------------------------------------
 #
-# Replace the dummy line in the XML defining the "at_start" cycle with a
-# line containing actual values.
-#
-#-----------------------------------------------------------------------
-#
-#regex_search="(^\s*<cycledef\s+group=\"at_start\">00)\s+(&HH_FIRST_CYCL;)\s+(&DD_FIRST_CYCL;)\s+(&MM_FIRST_CYCL;)\s+(&YYYY_FIRST_CYCL;)\s+(.*</cycledef>)(.*)"
-#regex_replace="\1 ${HH_FIRST_CYCL} ${DD_FIRST_CYCL} ${MM_FIRST_CYCL} ${YYYY_FIRST_CYCL} \6"
-#sed -i -r -e "s|${regex_search}|${regex_replace}|g" "$WFLOW_XML_FP"
-#
-#-----------------------------------------------------------------------
-#
 # Replace the dummy line in the XML defining a generic cycle hour with
 # one line per cycle hour containing actual values.
 #
